1342f3ed45c92cbb95cee7101280c7bc20b26fcb
skgladxe <skgladxe@gmail.com>
2026-07-24 00:20:28 +0530

    updatebugs

--- resources/views/website/partials/preloader.blade.php
+++ resources/views/website/partials/preloader.blade.php
@@ -2,6 +2,6 @@
 	<div class="preloader">
 		<div class="loading-container">
 			<div class="loading"></div>
-			<div id="loading-icon"><img src="{{ $siteLogoUrl ?? \App\Models\SiteSetting::websiteNavLogoUrl() }}" alt="{{ config('website.name') }}"></div>
+			<div id="loading-icon"><img src="{{ $siteFaviconUrl ?? \App\Models\SiteSetting::websiteNavLogoUrl() }}" alt="{{ config('website.name') }}"></div>
 		</div>
 	</div>
